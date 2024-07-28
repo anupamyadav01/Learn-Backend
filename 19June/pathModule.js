3847ac41f73637c33121eda44fdd6bfac3c29f3c
Anupam Yadav <anupamy571@gmail.com>
2024-07-28 10:46:11 +0530

    update

--- 19June/pathModule.js
+++ 19June/pathModule.js
@@ -8,4 +8,15 @@ const absolutePath = path.resolve("folder", "data.txt");
 // console.log(absolutePath);
 const baseName = path.basename(absolutePath);
 const dirname = path.dirname(absolutePath);
-console.log(dirname);
+// console.log(dirname);
+
+// absolute path end to end address
+// E:\Node Js\19June\folder
+
+// relative path
+// ../data.json
+
+const extensionName = path.extname(absolutePath);
+// console.log(extensionName);
+const pathDetails = path.parse(absolutePath);
+console.log(pathDetails);
